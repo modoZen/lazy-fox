73653d447f108f4b7c420955724750a43c81e080
Max <mdiazn@uni.pe>
2022-05-31 01:10:46 -0500

    clase24_neo

--- src/index.js
+++ src/index.js
@@ -1,38 +1,21 @@
-import { registerImage } from "./lazy";
+import { registerImage, resetLogState } from "./lazy";
+import { createImageNode } from "./utils";
 
 const API = 'https://randomfox.ca/floof/';
 const mountNode = document.querySelector('#images')
-const btnAdd = document.getElementById('addImg');
-const btnClean = document.getElementById('clean');
-
-const fetchUrl = async ()=> {
-    const response          = await fetch(API);
-    // const data      = await response.json();
-    // const url       = data.image;
-    const { image : url}    = await response.json();
-    return url;
-}
-
-const createImageNode = async ()=>{
-    const container = document.createElement("div");
-    container.classList.add('p-4');
-    const image = document.createElement("img");
-    image.dataset.src = await fetchUrl();
-    image.classList.add('mx-auto');
-    image.width = 320;
-    container.append(image);
-    return container
-}
+const btnAdd = document.querySelector('button[type="submit"]');
+const btnClean = document.querySelector('button[type="reset"]');
 
 const addImage = async ()=>{
-    const newImage = await createImageNode()
-    registerImage(newImage);
-    mountNode.append(newImage);
+    const [node, Image] = await createImageNode(API)
+    registerImage(Image);
+    mountNode.append(node);
 }
 
 btnAdd.addEventListener('click', addImage);
 
 btnClean.addEventListener('click', ()=>{
     mountNode.innerHTML = "";
+    resetLogState();
 })
     
--- src/lazy.js
+++ src/lazy.js
@@ -1,20 +1,35 @@
+let appendedImages = 0;
+let loadedImages = 0;
+
+const printLog = () => {
+    console.log(`⚪ Se han agregado ${appendedImages} imágenes`);
+    console.log(`🟣 Se han cargado ${loadedImages} imágenes`);
+    console.log("---------------------------------------");
+}
+
 const isIntersecting = (entry)=> entry.isIntersecting // true si esta dentro de la pantalla
 
 const loadImage = (entry)=>{
-    const container = entry.target;
-    const img = container.firstChild;
+    loadedImages++;
+    printLog()
+    const img = entry.target;
     img.src = img.dataset.src;
-    observer.unobserve(container);
+    observer.unobserve(img);
 }
 
 const observer = new IntersectionObserver((entries)=>{
     const entry = entries[0];
     if(isIntersecting(entry)) loadImage(entry)
-    // entries.filter(isIntersecting).forEach(accion);
-    // console.log(entries)
 });
 
 export const registerImage = (image) =>{
     // IntersectactionObservador -> observer(image)
+    appendedImages++;
+    printLog()
     observer.observe(image);
+}
+
+export const resetLogState = ()=>{
+    appendedImages = 0;
+    loadedImages = 0;
 }
