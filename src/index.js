923f00beb2a97c5d6dbc3204913f57d1deef93d9
Max <mdiazn@uni.pe>
2022-05-30 22:59:43 -0500

    clase24_extra

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import h from "hyperscript";
 import { registerImage } from './lazy';
 
 console.log('Happy hacking :)');
@@ -7,17 +8,18 @@ const btnAdd = document.getElementById('addImg');
 const btnClean = document.getElementById('clean');
 
 async function fetchData() {
-    const response  = await fetch(API);
-    const data      = await response.json();
-    const url       = data.image;
-    
-    const container = document.createElement("div");
-    container.classList.add('p-4');
-
-    const image = document.createElement("img");
-    image.dataset.src = url;
-    image.classList.add('mx-auto');
-    image.width = 320;
+    const response          = await fetch(API);
+    const { image : url }   = await response.json();
+    // const url       = data.image;
+      
+    // const image = document.createElement("img");
+    // image.dataset.src = url;
+    // image.classList.add('mx-auto');
+    // image.width = 320;
+    const image = h('img.mx-auto',{
+        width: 300,
+        'data-src': url
+    })
 
     const imageWrapper = document.createElement("div");
     imageWrapper.className = "bg-gray-300";
@@ -25,7 +27,12 @@ async function fetchData() {
     imageWrapper.style.display = "inline-block";
 
     imageWrapper.append(image);
-    container.append(imageWrapper);
+    // container.append(imageWrapper);
+
+     // const container = document.createElement("div");
+    // container.classList.add('p-4');
+    const container = h('div.p-4',imageWrapper);
+
     images.append(container);
     registerImage(container);
 }
