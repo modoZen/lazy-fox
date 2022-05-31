d2577c91f53caa63f39e7099971b9429ae95d70e
Max <mdiazn@uni.pe>
2022-05-30 23:35:45 -0500

    clase21_neo

--- src/index.js
+++ src/index.js
@@ -1,46 +1,35 @@
-import h from "hyperscript";
-import { registerImage } from './lazy';
-
-console.log('Happy hacking :)');
-
 const API = 'https://randomfox.ca/floof/';
+const mountNode = document.querySelector('#images')
 const btnAdd = document.getElementById('addImg');
 const btnClean = document.getElementById('clean');
 
-async function fetchData() {
+const fetchUrl = async ()=> {
     const response          = await fetch(API);
-    const { image : url }   = await response.json();
+    // const data      = await response.json();
     // const url       = data.image;
-      
-    // const image = document.createElement("img");
-    // image.dataset.src = url;
-    // image.classList.add('mx-auto');
-    // image.width = 320;
-    const image = h('img.mx-auto',{
-        width: 300,
-        'data-src': url
-    })
-
-    const imageWrapper = document.createElement("div");
-    imageWrapper.className = "bg-gray-300";
-    imageWrapper.style.minHeight = "100px";
-    imageWrapper.style.display = "inline-block";
-
-    imageWrapper.append(image);
-    // container.append(imageWrapper);
-
-     // const container = document.createElement("div");
-    // container.classList.add('p-4');
-    const container = h('div.p-4',imageWrapper);
-
-    images.append(container);
-    registerImage(container);
+    const { image : url}    = await response.json();
+    return url;
 }
 
-btnAdd.addEventListener('click', fetchData);
+const createImageNode = async ()=>{
+    const container = document.createElement("div");
+    container.classList.add('p-4');
+    const image = document.createElement("img");
+    image.src = await fetchUrl();
+    image.classList.add('mx-auto');
+    image.width = 320;
+    container.append(image);
+    return container
+}
+
+const addImage = async ()=>{
+    const newImage = await createImageNode()
+    mountNode.append(newImage);
+}
+
+btnAdd.addEventListener('click', addImage);
 
 btnClean.addEventListener('click', ()=>{
-    [...images.childNodes].forEach(child=>{
-        child.remove();
-    })
+    mountNode.innerHTML = "";
 })
+    
